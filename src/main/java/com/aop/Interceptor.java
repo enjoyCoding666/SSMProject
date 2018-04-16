af407d7c6447e8d395fb36e7e4209785818d06e2
linfeng <475534279@qq.com>
2018-04-16 21:26:18 +0800

    添加aop缺少的jar包

--- src/main/java/com/aop/Interceptor.java
+++ src/main/java/com/aop/Interceptor.java
@@ -4,7 +4,9 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
+@Component
 @Aspect
 public class Interceptor {
     /**
